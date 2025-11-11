c6012000236e14d317136e5f35bb54dc44f4a1af
sarah1negm <sarah001negm@gmail.com>
2025-11-11 09:41:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,38 +8,67 @@
 
 using namespace std;
 
-// ==================== Person Class Implementation =========================
-
-
-
-
-
-
-
-
-// ==================== Student Class Implementation ====================
-
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
+class Person{
+    protected:
+    String name;
+    int id=-1;
+    public:
+    Person(String name="unnamed", int id=++){
+        this.name=name;
+        this.id=id;
+    }
+    display(){
+        cout<< "Name: "<< name<< endl<<"ID: "<< id;
+    }
+}
+class Student: Person{
+    private:
+    int yearLevel;
+    String major;
+    public:
+    Student(String name="unnamed", int id=++,string major="untitled",int YearLevel=1):Person(name,id){
+        this.major=major;
+        this.YearLevel=YearLevel;
+    }
+    display():Person.display(){
+        cout<< "Year Level: "<< yearLevel<< endl<<"Major: "<< major;
+    }
+}
+class Instuctor:Person{
+    private:
+    String department;
+    int experienceYears;
+    public:
+    Instructor(String name="unnamed", int id=++, String name="unnamed", int id=++):Person(name,id){
+        this.name=name;
+        this.id=id;
+    }
+    display():Person.display(){
+        cout<< "department: "<< department<< endl<<"Years of experience : "<< experienceYears;
+    }
+}
+class Course{
+    private:
+    String courseCode;
+    String courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+    public:
+    addStudent(const Student& s){
+        if(currentStudents<maxStudents){
+             currentStudents++;
+             * (students+1)= s;
+        }
+        else
+            cout<<" cannot add student, exceeded maximum number of students.";
+    }
+    displayCourseInfo(){
+        cout<< "course Code: "<< courseCode<< endl<<" course Name: "<< courseName<< endl<<" max Students: "<<maxStudents << endl<< "current Students: "<<currentStudents;
+        for(int i= currentStudents-1; i>=0 ;i--)
+            cout<< endl<<*(students-i);
+    }
+}
 // ==================== Main Function ====================
 int main() {
    
