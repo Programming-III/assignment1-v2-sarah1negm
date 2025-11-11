814acec479fac3ab980f5400b06c9741cf5c3e0e
sarah1negm <sarah001negm@gmail.com>
2025-11-11 09:40:06 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -4,10 +4,28 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
-
-
-
+class Course{
+    private:
+    String courseCode;
+    String courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+    public:
+    addStudent(const Student& s){
+        if(currentStudents<maxStudents){
+             currentStudents++;
+             * (students+1)= s;
+        }
+        else
+            cout<<" cannot add student, exceeded maximum number of students.";
+    }
+    displayCourseInfo(){
+        cout<< "course Code: "<< courseCode<< endl<<" course Name: "<< courseName<< endl<<" max Students: "<<maxStudents << endl<< "current Students: "<<currentStudents;
+        for(int i= currentStudents-1; i>=0 ;i--)
+            cout<< endl<<*(students-i);
+    }
+}
 
 
 
