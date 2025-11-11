afa3f21522449ac23020333b930edc90330c5651
sarah1negm <sarah001negm@gmail.com>
2025-11-11 09:42:00 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -12,19 +12,8 @@ class Course{
     Student* students;
     int currentStudents;
     public:
-    addStudent(const Student& s){
-        if(currentStudents<maxStudents){
-             currentStudents++;
-             * (students+1)= s;
-        }
-        else
-            cout<<" cannot add student, exceeded maximum number of students.";
-    }
-    displayCourseInfo(){
-        cout<< "course Code: "<< courseCode<< endl<<" course Name: "<< courseName<< endl<<" max Students: "<<maxStudents << endl<< "current Students: "<<currentStudents;
-        for(int i= currentStudents-1; i>=0 ;i--)
-            cout<< endl<<*(students-i);
-    }
+    addStudent(const Student& s);
+    displayCourseInfo();
 }
 
 
