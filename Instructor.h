77392a712f6bcd42efe1f9bb75ac69dc39e9a241
sarah1negm <sarah001negm@gmail.com>
2025-11-11 09:39:39 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -2,18 +2,17 @@
 #define INSTRUCTOR_H
 
 #include "Person.h"
-using namespace std;
-//#write Instructor class here
-
-
-
-
-
-
-
-
-
-
-
-
+using namespace std;class Instuctor:Person{
+    private:
+    String department;
+    int experienceYears;
+    public:
+    Instructor(String name="unnamed", int id=++, String name="unnamed", int id=++):Person(name,id){
+        this.name=name;
+        this.id=id;
+    }
+    display():Person.display(){
+        cout<< "department: "<< department<< endl<<"Years of experience : "<< experienceYears;
+    }
+}
 #endif
